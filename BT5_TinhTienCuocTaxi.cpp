dcf1fd72e55deb4c972cf9d047987f74151d462e
huynguyen2219 <112061124+huynguyen2219@users.noreply.github.com>
2022-08-25 18:40:39 +0700

    Update BT5_TinhTienCuocTaxi.cpp

--- BT5_TinhTienCuocTaxi.cpp
+++ BT5_TinhTienCuocTaxi.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int Tinh_Cuoc_Taxi(int x) //Ham tinh tien cuoc taxi dua vo so km
 {
-	sum = 5000; // khoi tao bien sum luu so tien cuoc km dau tien
-	if (x>0 && x<=1)
+	int sum = 5000; // khoi tao bien sum luu so tien cuoc km dau tien
+	if (x > 0 && x <= 1)
 	{
 		return sum;
 	}
@@ -24,20 +24,25 @@ int Tinh_Cuoc_Taxi(int x) //Ham tinh tien cuoc taxi dua vo so km
 
 int main()
 {
-	do
+	int n; // so km 
+	while (1)
 	{
-		int n; // so km 
-		cout << "\n\n\tNhap so km di duoc: ";
-		cin >> n;
-		if (n <= 0)
+		system("cls");
+		do
 		{
-			cout<<"So km nhap vao khong hop le. Xin moi nhap lai"
-		}
-	} while (n <= 0);
+			
+			cout << "\n\n\tNhap so km di duoc: ";
+			cin >> n;
+			if (n <= 0)
+			{
+				cout << "So km nhap vao khong hop le. Xin moi nhap lai";
+			}
+		} while (n <= 0);
 
-	int sotien = Tinh_Cuoc_Taxi(n);
-	cout << "\n\n\tSo tien cuoc taxi cho " << n << " km la: " << sotien << "d";
-	
+		int sotien = Tinh_Cuoc_Taxi(n);
+		cout << "\n\n\tSo tien cuoc taxi cho " << n << " km la: " << sotien << "d"<<endl;
+		system("pause");
+	}
 
 
 }
